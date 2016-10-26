bd3bdaa016f82d96af491a98e4d1283a247c8a30
Maxim Kudinov <mkudinov@distillery.com>
2016-10-27 01:42:31 +0300

    CategoryBusiness

---
+++ Business/Controllers/CategoryBusiness.cs
@@ -0,0 +1,12 @@
+﻿using Data.Models;
+
+namespace Business.Controllers
+{
+    class CategoryBusiness: BaseBusiness<Category>
+    {
+        public CategoryBusiness(DataContext ctx) : base(ctx)
+        {
+
+        }
+    }
+}
