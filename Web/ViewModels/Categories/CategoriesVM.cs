6cd0c851293a11e1631f69b39a20f23a2eb5d701
Maxim Kudinov <mkudinov@distillery.com>
2016-10-27 00:30:24 +0300

    CategoriesVM

---
+++ Web/ViewModels/Categories/CategoriesVM.cs
@@ -0,0 +1,11 @@
+﻿using Data.Models;
+using System.Collections.Generic;
+
+namespace Web.ViewModels.Categories
+{
+    public class CategoriesVM
+    {
+        public Category Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
