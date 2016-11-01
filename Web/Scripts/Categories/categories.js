bc6f06469c1253a5dabb4fd07e694f9c5995be55
Maxim Kudinov <mkudinov@distillery.com>
2016-11-01 23:21:49 +0300

    context menu

---
+++ Web/Scripts/Categories/categories.js
@@ -0,0 +1,57 @@
+﻿$(function () {
+    $('#jstree').jstree({
+        "core": {
+            "check_callback": true
+        },
+        "types": {
+            "#": {
+                "max_children": 1,
+                "Valid_children": ["root"]
+            },
+            "root": {
+                "valid_children": ["folder"],
+                "icon": "/Content/jsTree/themes/default/root.png"
+            },
+            "folder": {
+                "valid_children": ["folder", "category"],
+                "icon": "/Content/jsTree/themes/default/folder.png"
+            },
+            "category": {
+                "valid_children": [],
+                "icon": "/Content/jsTree/themes/default/category.png"
+            }
+        },
+        "contextmenu": {
+            items: customMenu
+        },
+        "plugins": ["wholerow", "dnd", "types", "contextmenu"]
+    });
+
+    function customMenu(node) {
+        var items = {
+            create: {
+                "label": "Create",
+                "action": function (obj) {
+                }
+            },
+            rename: {
+                "label": "Rename",
+                "action": function (obj) {
+                }
+            },
+            deleteOption: {
+                "label": "Delete",
+                "action": function (obj) {
+                }
+            }
+        }
+
+        if (node.type == "root") {
+            delete items.create;
+            delete items.rename;
+            delete items.deleteOption;
+        }
+
+        return items;
+    }
+});
