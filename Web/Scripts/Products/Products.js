bd128c7058439126f157237519f1d739ad2c47ed
Maxim Kudinov <mkudinov@distillery.com>
2016-11-08 01:10:44 +0300

    checkboxes

---
+++ Web/Scripts/Products/Products.js
@@ -0,0 +1,29 @@
+﻿$(function () {
+    $('#jstree').jstree({
+        "core": {
+            "check_callback": true
+        },
+        "types": {
+            "#": {
+                "max_children": 1,
+                "Valid_children": ["root"]
+            },
+            "root": {
+                "valid_children": ["folder", "leaf"],
+                "icon": "/Content/jsTree/themes/default/root.png"
+            },
+            "folder": {
+                "valid_children": ["folder", "leaf"],
+                "icon": "/Content/jsTree/themes/default/folder.png"
+            },
+            "leaf": {
+                "valid_children": ["leaf"],
+                "icon": "/Content/jsTree/themes/default/leaf.png"
+            }
+        },
+        "plugins": ["wholerow", "types", "checkbox"]
+    });
+
+    var tree = $("#jstree").jstree(true);
+
+});
