75e3389efbe8fb7cbe8431f816893c9ae46adf63
Maxim Kudinov <mkudinov@distillery.com>
2016-12-23 14:17:21 +0300

    datamember attribute

--- Business/Controllers/CategoryBusiness.cs
+++ Business/Controllers/CategoryBusiness.cs
@@ -61,14 +61,14 @@ namespace Business.Controllers
         {
             int? nodeId = null;
 
-            if (node.type != "root")
+            if (node.Type != "root")
             {
                 //jsTree provides unparsable ids for js created nodes (that are not in the DB)
                 //so if we can't parse the id then this category should be inserted
                 int nodeIdParsed;
-                if (!int.TryParse(node.id, out nodeIdParsed))
+                if (!int.TryParse(node.Id, out nodeIdParsed))
                 {
-                    var createdCategory = _Ctx.Categories.Add(new Category { Name = node.text, ParentId = parentId, CreatedDate = DateTime.UtcNow });
+                    var createdCategory = _Ctx.Categories.Add(new Category { Name = node.Text, ParentId = parentId, CreatedDate = DateTime.UtcNow });
                     //need to save changes to get new id
                     _Ctx.SaveChanges();
                     nodeId = createdCategory.Id;
@@ -81,9 +81,9 @@ namespace Business.Controllers
 
                     //the checks below will help us to avoid unnecessary SQL script while saving the changes
 
-                    if (dbCategory.Name != node.text)
+                    if (dbCategory.Name != node.Text)
                     {
-                        dbCategory.Name = node.text;
+                        dbCategory.Name = node.Text;
                     }
 
                     if (dbCategory.ParentId != parentId)
@@ -95,9 +95,9 @@ namespace Business.Controllers
                 }
             }
 
-            if (node.children != null)
+            if (node.Children != null)
             {
-                foreach (var childNode in node.children)
+                foreach (var childNode in node.Children)
                 {
                     if (!TrySaveTree(childNode, nodeId)) return false;
                 }
@@ -124,12 +124,12 @@ namespace Business.Controllers
             foreach (var node in nodes)
             {
                 int id = 0;
-                if (int.TryParse(node.id, out id))
+                if (int.TryParse(node.Id, out id))
                 {
                     yield return id;
                 }
 
-                foreach (var child_id in GetNodesIds(node.children))
+                foreach (var child_id in GetNodesIds(node.Children))
                 {
                     yield return child_id;
                 }
--- Data/Models/jsTree/JsTreeModel.cs
+++ Data/Models/jsTree/JsTreeModel.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace Data.Models.jsTree
 {
     //names should be in lower case to match json data
     public class JsTreeModel
     {
-        public string id { get; set; }
-        public string text { get; set; }
-        public string type { get; set; }
-        public ICollection<JsTreeModel> children { get; set; }
+        [DataMember(Name = "id")]
+        public string Id { get; set; }
+
+        [DataMember(Name ="text")]
+        public string Text { get; set; }
+
+        [DataMember(Name ="type")]
+        public string Type { get; set; }
+
+        [DataMember(Name ="children")]
+        public ICollection<JsTreeModel> Children { get; set; }
     }
 }
